601f395692eb757d8e9ea589734b1a38d74e9b4f
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-04-29 17:02:45 +0300

    Initial Commit

---
+++ Tests/DataGeneratorTests/DataGeneratorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import DataGenerator
+
+final class DataGeneratorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DataGenerator().text, "Hello, World!")
+    }
+}
